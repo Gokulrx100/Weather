8f205694cf83a28360abd0ae3f80c37a7624c640
Gokulrx100 <gokulkrishnakumar04@gmail.com>
2025-05-25 22:32:07 +0530

    basic struture of home page and nav

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('nav button').forEach(btn => {
+    btn.addEventListener('click', function() {
+        document.querySelectorAll('nav button').forEach(b => b.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
+
+function navigate(view) {
+  document.querySelectorAll('.view').forEach(sec => sec.classList.add('hidden'));
+  document.getElementById(view).classList.remove('hidden');
+}
